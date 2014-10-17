a9805b7775903b5f765a3d28b39f64f8111b2178
Brian Kelly <brian.kelly@golgi.io>
2014-10-17 12:26:02 -0700

    Use environment variable for identity

--- Server/gct.sh
+++ Server/gct.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ "$GCT_NAME" = "" ]; then
+    echo "You must specify an endpoint name in the GCT_NAME environmant variable"
+    exit -1
+fi
+
 if [ $# -lt 2 ]; then
 	echo "Usage: $0 <server-identity> <cmd> [arg1 [arg2...]]";
 	exit -1
@@ -8,4 +13,4 @@ fi
 TARGET=$1
 shift
 
-java -classpath gct.jar:$HOME/Golgi-Pkg/LATEST//common/golgi_j2se.jar:libs/log4j-1.2.17.jar:libs/httpclient-4.3.3.jar io.golgi.gct.Client -devKey `cat ../Golgi.DevKey` -appKey `cat ../Golgi.AppKey` -identity `hostname`-CLI -target $TARGET -- $*
+java -classpath gct.jar:$HOME/Golgi-Pkg/LATEST//common/golgi_j2se.jar:libs/log4j-1.2.17.jar:libs/httpclient-4.3.3.jar io.golgi.gct.Client -devKey `cat ../Golgi.DevKey` -appKey `cat ../Golgi.AppKey` -identity $GCT_NAME -target $TARGET -- $*
