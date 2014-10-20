d93baa8a8313f663df2942f215d6b4fb956e329c
Brian Kelly <brian.kelly@golgi.io>
2014-10-20 16:24:24 -0700

    Refactored to include fput and fget

--- GolgiCommandTunnel.thrift
+++ GolgiCommandTunnel.thrift
@@ -31,25 +31,54 @@ namespace java io.golgi.gct.gen
 //
 
 struct CommandDetails{
-    1: required string lclKey,
+    1: required string cliKey,
     2: required string cmdLine,
 }
 
+struct FgetDetails{
+    1: required string cliKey,
+    2: required string filename,
+}
+
+struct FputDetails{
+    1: required string cliKey,
+    2: required string filename,
+}
+
+struct FputParams{
+    1: required string svrKey,
+}
+
+exception FputException{
+    1: required string cliKey,
+    2: required i32 errCode,
+    3: required string errText,
+}
+
 struct OutputPacket{
-    1: required string lclKey,
+    1: required string dstKey,
     2: required i32 fd,
     3: required i32 pktNum,
     4: required data data,
 }
 
 struct TermStatus{
-    1: required string lclKey,
+    1: required string key,
     2: required i32 pktTotal,
     3: required i32 exitCode,
+    4: required string errorText,
 }
 
 service  GCT{
+    void moreData(1:OutputPacket pkt),
+
     void launchCommand(1:CommandDetails cmdDetails),
-    void commandOutput(1:OutputPacket pkt),
     void commandComplete(1:TermStatus termStatus),
+
+    void fget(1:FgetDetails fgetDetails),
+    void fgetComplete(1:TermStatus termStatus),
+
+    FputParams fputStart(1:FputDetails fputDetails)
+		   throws(1:FputException fpe),
+    TermStatus fputComplete(1:TermStatus termStatus),
 }
